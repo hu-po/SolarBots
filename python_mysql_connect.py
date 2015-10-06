48ced2fb4dcc313ef99bd3d734271bd9827490fc
Hugo-WMac <Hugo-WMac@users.noreply.github.com>
2015-10-06 15:52:59 -0400

    Added slam code

--- python_mysql_connect.py
+++ python_mysql_connect.py
@@ -47,13 +47,56 @@ def insert_sensor_data(data):
         cursor.close()
         conn.close()
 
+def insert_current_pos():
 
-def query_commands():
+    # Table info: SensorData(sensorType varchar(255), sensorNum int, reading int, Date DATETIME(6))
+
+    query = "INSERT INTO Localization(type, x_pos, y_pos, theta) VALUES(%s, %d, %d, %d)"
+
+    try:
+        db_config = read_db_config()
+        conn = MySQLConnection(**db_config)
+
+        cursor = conn.cursor()
+        cursor.executemany(query, data)
+
+        conn.commit()
+    except Error as e:
+        print('Error:', e)
+
+    finally:
+        cursor.close()
+        conn.close()
+
+def query_map():
     try:
         dbconfig = read_db_config()
         conn = MySQLConnection(**dbconfig)
         cursor = conn.cursor()
-        cursor.execute("SELECT * FROM commands")
+        cursor.execute("SELECT x_pos, y_pos FROM Map") # TODO: Make consistent with database
+
+        row = cursor.fetchall()
+
+        while row is not None:
+            print(row)
+            row = cursor.fetchall()
+
+    except Error as e:
+        print(e)
+
+    finally:
+        cursor.close()
+        conn.close()
+
+    return row
+
+
+def query_current_pos():
+    try:
+        dbconfig = read_db_config()
+        conn = MySQLConnection(**dbconfig)
+        cursor = conn.cursor()
+        cursor.execute("SELECT x_pos, y_pos, theta FROM Localization WHERE type = robotpose") # TODO: Make consistent with database
 
         row = cursor.fetchone()
 
@@ -72,6 +115,7 @@ def query_commands():
 
 
 def main():
+
     connect()
 
     data = [('test', 1, 10.0, datetime.datetime.now()),
